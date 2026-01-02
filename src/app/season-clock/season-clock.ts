c3e2ab27b25d5a4b3b5601f511751327244eb88d
Your Name <dan.weibren@gmail.com>
2026-01-02 11:30:51 +0200

    feat(season-clock): implement component logic and UI

---
+++ src/app/season-clock/season-clock.ts
@@ -0,0 +1,56 @@
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
+
+@Component({
+  selector: 'season-clock',
+  standalone: false,
+  templateUrl: './season-clock.html',
+  styleUrl: './season-clock.scss',
+})
+export class SeasonClock implements OnInit, OnDestroy {
+  title = 'Season Clock'
+
+  now: Date = new Date()
+  formattedTime: string = ''
+  seasonName: string = ''
+  seasonImg: string = ''
+  isDark: boolean = false
+
+  private intervalId!: number;
+
+
+  onToggleDarkMode(ev: MouseEvent) {
+    this.isDark = !this.isDark
+  }
+
+  geSeasonName() {
+    let month = this.now.getMonth()
+
+    if (month >= 2 && month <= 4) {
+      this.seasonImg = '🌸'
+      return this.seasonName = 'Spring'
+    }   // Mar, Apr, May
+    if (month >= 5 && month <= 7) {
+      this.seasonImg = '🌞'
+      return this.seasonName = 'Summer'
+    }   // Jun, Jul, Aug
+    if (month >= 8 && month <= 10) {
+      this.seasonImg = '🍂'
+      return this.seasonName = 'Autumn'
+    }  // Sep, Oct, Nov
+    this.seasonImg = '❄️'
+    return this.seasonName = 'winter'
+  }
+
+
+  ngOnInit(): void {
+    this.geSeasonName()
+    this.intervalId = window.setInterval(() => {
+      this.now = new Date();
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId!)
+  }
+
+}
